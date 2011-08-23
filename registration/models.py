f7740f7f5c3ae275de499ec94eb7275f8e310673
Nikolay Amiantov <nikoamia@gmail.com>
2011-08-23 17:26:32 +0400

    Fixed bug with registration.

--- registration/models.py
+++ registration/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 from django.conf import settings
 from django.db.models.signals import post_save
 
@@ -9,10 +9,6 @@ def user_get_absolute_url(self):
 
 User.get_absolute_url = user_get_absolute_url
 
-def post_user_create(instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
-
 def default_group_append(instance, created, **kwargs):
     if created and settings.ADD_USERS_TO_DEFAULT_GROUP:
         name = settings.DEFAULT_GROUP_NAME
@@ -20,4 +16,3 @@ def default_group_append(instance, created, **kwargs):
         instance.groups.add(group)
 
 post_save.connect(default_group_append, sender=User)
-post_save.connect(post_user_create, sender=User)
