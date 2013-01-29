2ee05d8ead9dbb05c0b70433026cd26280c85186
Andrey Shadrikov <notvuvko@gmail.com>
2013-01-29 21:31:37 +0400

    ???????? ??????? ????????? (????????? ???????? ?????????).

--- messages/urls.py
+++ messages/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns('',
     url(r'create/$', views.MessageCreateView.as_view(), name='message_create'),
     url(r'detail/(?P<pk>\d+)/$', views.MessageDetailView.as_view(), name='message_detail'),
     url(r'update/(?P<pk>\d+)/$', views.MessageUpdateView.as_view(), name='message_update'),
-    url(r'delete/(?P<pk>\d+)/$', views.MessageDeleteView.as_view(success_url=reverse_lazy('message_received')), name='message_delete'),
+    url(r'delete/(?P<pk>\d+)/$', views.MessageDeleteView.as_view(success_url=reverse_lazy('message_delete')), name='message_delete'),
 )
